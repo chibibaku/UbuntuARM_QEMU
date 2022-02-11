1f368d8ee7fc2061ea6397194d0b9ef7af96118b
chibibaku <chibibaku@outlook.jp>
2022-02-11 21:07:48 +0900

    RC 1 Build

--- mkvm.sh
+++ mkvm.sh
@@ -22,6 +22,8 @@ if [ "$password" = "" ] ; then
   exit
 fi
 
+echo ""
+
 echo "Update package lists..."
 sudo apt update &> /dev/null
 echo "Install Qemu..."
@@ -29,15 +31,21 @@ sudo apt install -y qemu-system-arm &> /dev/null
 echo "Install cloud-image-utils..."
 sudo apt install -y cloud-image-utils &> /dev/null
 
+echo ""
+
 echo "Wget UbuntuARM Image... (Its will takes few minutes)"
 wget -O ubuntuARM.img http://ubuntutym2.u-toyama.ac.jp/cloud-images/releases/18.04/release/ubuntu-18.04-server-cloudimg-arm64.img &> /dev/null
 echo "Wget QemuEFI file..."
 wget https://releases.linaro.org/components/kernel/uefi-linaro/latest/release/qemu64/QEMU_EFI.fd &> /dev/null
 
+echo ""
+
 echo "Make EFI as mountable image file..."
 dd if=/dev/zero of=flash0.img bs=1M count=64 &> /dev/null
 dd if=QEMU_EFI.fd of=flash0.img conv=notrunc &> /dev/null
 dd if=/dev/zero of=flash1.img bs=1M count=64 &> /dev/null
+
+echo ""
 
 echo "Generating cloud-init file from Username & Password..."
 touch cloud.txt
@@ -48,11 +56,17 @@ echo "chpasswd: { expire: False }" >> cloud.txt
 echo "ssh_pwauth: True" >> cloud.txt
 cloud-localds --disk-format qcow2 cloud.img cloud.txt &> /dev/null
 
-echo "Configure start.sh"
+echo ""
+
+echo "Configure start.sh..."
 chmod +x ./start.sh
+
+echo ""
 
 echo "Remove trash file..."
 rm cloud.txt
 #sudo apt purge -y cloud-image-utils
+
+echo ""
 
 echo "All tasks done! ./start.sh to start VM"
